062e672144aba3ef059ad535ab0f6e8200b16521
Luiz Felipe <luizfelipesoma@gmail.com>
2023-05-17 09:15:16 -0300

    Outra branch

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Image from "react-bootstrap/Image";
 // import Figure from "react-bootstrap/Figure"
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 import "../Sass/header.scss";
 
@@ -20,7 +20,11 @@ const Header = () => {
                 <Nav id="nav-right">
                     <Nav.Link href="#sobre">Sobre</Nav.Link>
                     <Nav.Link href="#contato">Contato</Nav.Link>
-                    <Nav.Link> <Button href="#login" size="sm" id="btLogin"><ion-icon name="person-outline"></ion-icon><span>entrar</span></Button></Nav.Link>
+                    <Nav.Link> <Button href="#login" size="sm" id="btLogin">
+                        <ion-icon name="person-outline">
+                        </ion-icon><span>entrar</span>
+                    </Button>
+                    </Nav.Link>
                 </Nav>
             </Nav>
         </Navbar>
